2d2d8e7a5129a3ae982b82594f48b608701853e0
Vaishnudevi86 <88608139+Vaishnudevi86@users.noreply.github.com>
2021-08-14 04:23:54 -0400

    ChangeOddIndexToUpperCase

---
+++ ChangeOddIndexToUpperCase.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class ChangeOddIndexToUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "changeme";
+		char ch[] = test.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+
+			if (i % 2 != 0) {
+
+				System.out.println(Character.toUpperCase(ch[i]));
+			} else
+				System.out.println(ch[i]);
+
+		}
+	}
+}
